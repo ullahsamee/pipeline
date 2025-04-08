c38855f79f9d789d3ffd3596c8722f1a640d451d
Samee Ullah <sameeullah@bs.qau.edu.pk>
2025-04-08 16:24:22 -0500

    Update run_rfdiff.sh

--- src/scripts/run_rfdiff.sh
+++ src/scripts/run_rfdiff.sh
@@ -1,12 +1,20 @@
-# This is the bash script used to generate binders to the cd20 domains focussed on hotspot residues 168 to 175
-docker run -it --rm --gpus all \
-   -v $HOME/models:$HOME/models \
-   -v $HOME/inputs:$HOME/inputs \
-   -v $HOME/outputs:$HOME/outputs \
-   rfdiffusion \
-   inference.output_prefix=$HOME/outputs/binder_design22 \
-   inference.model_directory_path=$HOME/models \
-   inference.input_pdb=$HOME/inputs/cd20.pdb \
-   inference.num_designs=10 \
-   'contigmap.contigs=[C46-210/0 D46-210/0 80-80]' \
-   'ppi.hotspot_res=[C168,C169,C170,C171,C172,C173,C174,C175,D168,D169,D170,D171,D172,D173,D174,D175]'
+#!/bin/bash
+set -e
+
+echo "Running RFDiffusion Docker container with project paths..."
+
+docker run --rm --gpus all \
+  -v $(pwd)/models:/app/models \
+  -v $(pwd)/data/pdb:/app/inputs \
+  -v $(pwd)/data/output:/app/outputs \
+  rfdiffusion \
+  inference.output_prefix=/app/outputs/binder_design_rfdiff \
+  inference.model_directory_path=/app/models \
+  inference.input_pdb=/app/inputs/cd20.pdb \
+  inference.num_designs=10 \
+  inference.ckpt_override_path=/app/models/Complex_base_ckpt.pt \
+  'contigmap.contigs=[C46-210/0 D46-210/0 80-80]' \
+  'ppi.hotspot_res=[C168,C169,C170,C171,C172,C173,C174,C175,D168,D169,D170,D171,D172,D173,D174,D175]'
+  # NO backslash on this last line ^
+
+echo "RFDiffusion Docker container finished."
